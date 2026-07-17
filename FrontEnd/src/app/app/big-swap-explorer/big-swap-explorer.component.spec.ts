031ade9f58bd8a2662c1591d04150d849ae8a3a4
Heyther <htrsanbl@gmail.com>
2026-07-16 20:00:38 -0500

    fix component tests: add HttpClientTestingModule to BigSwapExplorer, add MatIconModule to TimeCounter

--- FrontEnd/src/app/app/big-swap-explorer/big-swap-explorer.component.spec.ts
+++ FrontEnd/src/app/app/big-swap-explorer/big-swap-explorer.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { BigSwapExplorerComponent } from './big-swap-explorer.component';
 
@@ -8,7 +9,8 @@ describe('BigSwapExplorerComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [BigSwapExplorerComponent]
+      declarations: [BigSwapExplorerComponent],
+      imports: [HttpClientTestingModule]
     });
     fixture = TestBed.createComponent(BigSwapExplorerComponent);
     component = fixture.componentInstance;
